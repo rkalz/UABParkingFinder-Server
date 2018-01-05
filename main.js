d92cf83edc9ba0c3575c179a47f48a833b39c9e1
Rofael Aleezada <me@rofael.com>
2018-01-05 07:03:59 -0600

    Fixed bug where overall status wasn't calculated correctly. Added fixes recommended by @OzairP

--- main.js
+++ main.js
@@ -1,42 +1,39 @@
-var express = require("express");
-var app = express();
-
+// Required to get Firebase working in Heroku
+const express = require("express");
+let app = express();
 app.set('port', (process.env.PORT || 5000));
 
-var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
-
+// Get Firebase credentials and initialize admin access
+let admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 console.log("Initializing Firebase SDK...");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://uab-parking-buddy.firebaseio.com"
 });
 
+// Get access to the database
 console.log("Initializing database access...");
-var db = admin.database();
-var ref = db.ref("reports");
+let db = admin.database();
+let ref = db.ref("reports");
 
-console.log("Listening to changes in database...");
-app.listen(app.get('port'), function() {
-  ref.on("child_changed", function(snapshot) {
-    var changedChild = snapshot.val();
-    var newest = changedChild[ Object.keys(changedChild).pop() ];
-    console.log("Time: " + newest.reportTime + " Lot: " + newest.lot + " Status: " + newest.status);
-    updateOverall(changedChild,newest.lot);
-  }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
-});
-
+// Looks at the ten most recent reports and determine the
+// overall lot status based on this
 function updateOverall(changedChild,lotName)
 {
   ref = db.ref("overall");
-  var total = 0;
-  var reportCount = 0;
+  let total = 0;
+  let reportCount = 0;
+  let reversedReportList = new Array();
 
-  for (var key in changedChild) {
-    if (changedChild.hasOwnProperty(key)){
-      total = total + changedChild[key].status;
+  // Dictionary is sorted oldest to newest, need reverse order
+  for (let key in changedChild) {
+    reversedReportList.unshift(key);
+  }
+
+  for (let index in reversedReportList) {
+    if (changedChild.hasOwnProperty(reversedReportList[index]) && reportCount != 10){
+      total += changedChild[reversedReportList[index]].status;
       reportCount++;
     }
   }
@@ -47,5 +44,19 @@ function updateOverall(changedChild,lotName)
     [lotName]: total
   });
 
+  // Change reference back to reports so we can listen for changes
   ref = db.ref("reports");
 }
+
+console.log("Listening to changes in database...");
+// Get the lot that has changed and perform updateOverall
+app.listen(app.get('port'), () => {
+  ref.on("child_changed", snapshot => {
+    var changedChild = snapshot.val();
+    var newest = changedChild[ Object.keys(changedChild).pop() ];
+    console.log("Time: " + newest.reportTime + " Lot: " + newest.lot + " Status: " + newest.status);
+    updateOverall(changedChild,newest.lot);
+  }, errorObject => {
+    console.log("The read failed: " + errorObject.code);
+  });
+});
